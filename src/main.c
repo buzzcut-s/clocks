c02e7872fab62d2ee337fe59bfe5959981caa173
Piyush Kumar <piyushkumg@gmail.com>
2021-09-04 21:23:56 +0530

    Implement disassembler

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
+#include <clocks/chunk.h>
+#include <clocks/common.h>
+#include <clocks/debug.h>
 
 int main(int argc, const char* argv[])
 {
-    printf("Witness.");
+    Chunk chunk;
+
+    init_chunk(&chunk);
+    write_chunk(&chunk, OP_RETURN);
+
+    disassemble_chunk(&chunk, "test chunk");
+    free_chunk(&chunk);
 
     return 0;
 }
