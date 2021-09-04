085159d0bbe432d928fd5a77e533520719060cfa
Piyush Kumar <piyushkumg@gmail.com>
2021-09-05 00:15:52 +0530

    chunk: Add line information array

--- src/main.c
+++ src/main.c
@@ -7,11 +7,11 @@ int main(int argc, const char* argv[])
     Chunk chunk;
 
     init_chunk(&chunk);
-    write_chunk(&chunk, OP_RETURN);
+    write_chunk(&chunk, OP_RETURN, 123);
 
     int constant = add_constant(&chunk, 21.03);
-    write_chunk(&chunk, OP_CONSTANT);
-    write_chunk(&chunk, constant);
+    write_chunk(&chunk, OP_CONSTANT, 123);
+    write_chunk(&chunk, constant, 123);
 
     disassemble_chunk(&chunk, "test chunk");
     free_chunk(&chunk);
