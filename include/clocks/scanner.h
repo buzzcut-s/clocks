ead4e46adf26b172b49040ba9bb825a8762447c3
Piyush Kumar <piyushkumg@gmail.com>
2021-09-05 13:02:04 +0530

    scanner: Add Token and TokenType structs

--- include/clocks/scanner.h
+++ include/clocks/scanner.h
@@ -1,6 +1,38 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+// clang-format off
+typedef enum {
+    // Single-character tokens.
+    TokenLeftParen, TokenRightParen,
+    TokenLeftBrace, TokenRightBrace,
+    TokenComma, TokenDot, TokenMinus, TokenPlus,
+    TokenSemicolon, TokenSlash, TokenStar,
+    // One or two character tokens.
+    TokenBang, TokenBangEqual,
+    TokenEqual, TokenEqualEqual,
+    TokenGreater, TokenGreaterEqual,
+    TokenLess, TokenLessEqual,
+    // Literals.
+    TokenIdentifier, TokenString, TokenNumber,
+    // Keywords.
+    TokenAnd, TokenClass, TokenElse, TokenFalse,
+    TokenFor, TokenFun, TokenIf, TokenNil, TokenOr,
+    TokenPrint, TokenReturn, TokenSuper, TokenThis,
+    TokenTrue, TokenVar, TokenWhile,
+
+    TokenError, TokenEof
+} TokenType;
+// clang-format on
+
+typedef struct
+{
+    TokenType   type;
+    const char* start;
+    int         length;
+    int         line;
+} Token;
+
 void init_scanner(const char* source);
 
 #endif  // SCANNER_H
