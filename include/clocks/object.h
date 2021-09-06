f3ab5be71e14456f33a8a253078f141049cf4f99
Piyush Kumar <piyushkumg@gmail.com>
2021-09-06 15:00:23 +0530

    object: Implement ObjString and helpers
    - See C Spec : § 6.7.2.1 13
    - Given an ObjString*, we can safely cast it to Obj* and then access the type field from it.
    - Given an Obj*, we can also downcast it to an ObjString*, provided the pointer we have does point to the obj field of an actual ObjString.
    - Pretty fucking cool, huh.

--- include/clocks/object.h
+++ include/clocks/object.h
@@ -1,6 +1,8 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include "value.h"
+
 typedef enum
 {
     ObjTypeString,
@@ -12,5 +14,21 @@ struct Obj
 };
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
+
+struct ObjString
+{
+    Obj   obj;
+    int   length;
+    char* chars;
+};
+
+#define IS_STRING(value)  is_obj_type(&(value), ObjTypeString)
+#define AS_STRING(value)  ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
+
+static inline bool is_obj_type(const Value* value, const ObjType type)
+{
+    return IS_OBJ(*value) && AS_OBJ(*value)->type == type;
+}
 
 #endif  // OBJECT_H
