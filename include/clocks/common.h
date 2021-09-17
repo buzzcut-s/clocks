8373519dbcdb7cc521737149d8621b023d25d401
Piyush Kumar <piyushkumg@gmail.com>
2021-09-17 20:31:43 +0530

    common: Rename debugging flag

--- include/clocks/common.h
+++ include/clocks/common.h
@@ -5,9 +5,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define DEBUG_ON
+#define CLOCKS_DEBUG
 
-#ifdef DEBUG_ON
+#ifdef CLOCKS_DEBUG
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 #define DEBUG_STRESS_GC
