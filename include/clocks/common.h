f0d8c71eb0be82e93fc4be35021a9066b23ce006
Piyush Kumar <piyushkumg@gmail.com>
2021-09-17 20:01:55 +0530

    common: Make toggling all debugging macros at once easier

--- include/clocks/common.h
+++ include/clocks/common.h
@@ -5,12 +5,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_ON
+
+#ifdef DEBUG_ON
 #define DEBUG_PRINT_CODE
-
 #define DEBUG_TRACE_EXECUTION
-
 #define DEBUG_STRESS_GC
 #define DEBUG_LOG_GC
+#endif
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
