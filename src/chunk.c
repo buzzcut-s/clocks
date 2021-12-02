buzzcut-s/clocks

commit 64c33f5cbcc355d827b4bf578f487f9473cc1bf2

chunk: Implement run-length encoding for line information
- Currently, our encoding of line information is hilariously wasteful of memory. Given that a series of instructions often correspond to the same source line, a natural solution is something akin to run-length encoding of the line numbers.
- Each LineStart struct marks the beginning of a new source line in the code, and the corresponding byte offset of the first instruction on that line. Any bytes after that first one are understood to be on that same line, until we hit the next LineStart.
- In each Chunk, we store an array of these structs.
- Separate line_count and line_capacity members are also added since these will be different from the code's - much shorter. That's the goal!
- Then in write_chunks, we make three changes.
- First, we don't implicitly grow the line array when we grow the code array. Their sizes are decoupled now. Instead, we grow the line array when appending a new LineStart, if needed.
- The second if statement is where we take advantage of adjacent instructions on the same line. If the line for the byte we're writing is on the same line as the current line start, we don't create a new one. This is the compression.
- Otherwise, if this is the first byte of code, or it appears on a different line, we begin a new LineStart and grow the array if needed.
- This gives us a compressed array of LineStarts, where each one begins a new line.
- For lookup, we use binary search since for a single pass compiler like clocks line numbers are always monotonically increasing.
- Given a byte offset for an instruction, get_line() binary searches through the LineStart array to find which LineStart - and thus which line - contains that offset. Use these for disassembling and during stack traces.

src/chunk.c
@@ -11,14 +11,22 @@ void init_chunk(Chunk* chunk)
     chunk->count    = 0;
     chunk->capacity = 0;
     chunk->code     = NULL;
-    chunk->lines    = NULL;
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+    chunk->line_count    = 0;
+    chunk->line_capacity = 0;
+#endif
+    chunk->lines = NULL;
     init_value_array(&chunk->constants);
 }
 
 void free_chunk(Chunk* chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+    FREE_ARRAY(LineStart, chunk->lines, chunk->line_capacity)
+#else
     FREE_ARRAY(int, chunk->lines, chunk->capacity);
+#endif
     free_value_array(&chunk->constants);
     init_chunk(chunk);
 }
@@ -32,14 +40,56 @@ void write_chunk(Chunk* chunk, const uint8_t byte, const int line)
         chunk->capacity = GROW_CAPACITY(old_capacity);
         chunk->code     = GROW_ARRAY(uint8_t, chunk->code,
                                      old_capacity, chunk->capacity);
-        chunk->lines    = GROW_ARRAY(int, chunk->lines,
-                                     old_capacity, chunk->capacity);
+#ifndef CHUNK_LINE_RUN_LENGTH_ENCODING
+        chunk->lines = GROW_ARRAY(int, chunk->lines,
+                                  old_capacity, chunk->capacity);
+#endif
     }
 
-    chunk->code[chunk->count]  = byte;
-    chunk->lines[chunk->count] = line;
+    chunk->code[chunk->count] = byte;
     chunk->count++;
+
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+    if (chunk->line_count > 0 && chunk->lines[chunk->line_count - 1].line == line)
+        return;
+
+    if (chunk->line_capacity < chunk->line_count + 1)
+    {
+        const int old_capacity = chunk->line_capacity;
+        chunk->line_capacity   = GROW_CAPACITY(old_capacity);
+        chunk->lines           = GROW_ARRAY(LineStart, chunk->lines,
+                                            old_capacity, chunk->line_capacity);
+    }
+
+    LineStart* line_start = &chunk->lines[chunk->line_count++];
+    line_start->offset    = chunk->count - 1;
+    line_start->line      = line;
+#else
+    chunk->lines[chunk->count] = line;
+#endif
 }
+
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+int get_line(const Chunk* chunk, const int instr_offset)
+{
+    int start = 0;
+    int end   = chunk->line_count - 1;
+
+    while (true)
+    {
+        const int mid = (start + end) / 2;
+
+        const LineStart* current = &chunk->lines[mid];
+
+        if (instr_offset < current->offset)
+            end = mid + 1;
+        else if (mid == chunk->line_count - 1 || instr_offset < chunk->lines[mid + 1].offset)
+            return current->line;
+        else
+            start = mid + 1;
+    }
+}
+#endif
 
 int add_constant(Chunk* chunk, const Value value)
 {

include/clocks/chunk.h
@@ -45,18 +45,36 @@ typedef enum
     OpMethod,
 } OpCode;
 
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+typedef struct
+{
+    int offset;
+    int line;
+} LineStart;
+#endif
+
 typedef struct
 {
     int        count;
     int        capacity;
     uint8_t*   code;
-    int*       lines;
     ValueArray constants;
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+    int        line_count;
+    int        line_capacity;
+    LineStart* lines;
+#else
+    int* lines;
+#endif
 } Chunk;
 
 void init_chunk(Chunk* chunk);
 void free_chunk(Chunk* chunk);
 void write_chunk(Chunk* chunk, uint8_t byte, int line);
+
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+int get_line(const Chunk* chunk, int offset);
+#endif
 
 int add_constant(Chunk* chunk, Value value);
 

include/clocks/common.h
@@ -20,6 +20,7 @@
 #define VM_CACHE_IP
 #define VALUE_NAN_BOXING
 
+#define CHUNK_LINE_RUN_LENGTH_ENCODING
 #define GC_OPTIMIZE_CLEARING_MARK
 #define OBJECT_CACHE_CLASS_INITIALIZER
 #define OBJECT_STRING_FLEXIBLE_ARRAY

src/debug.c
@@ -84,10 +84,19 @@ int disassemble_instruction(const Chunk* chunk, const int offset)
 {
     printf("%04d ", offset);
 
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+    const int line = get_line(chunk, offset);
+    if (offset > 0 && line == get_line(chunk, offset - 1))
+        printf("   | ");
+    else
+        printf("%4d ", line);
+#else
+
     if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
         printf("   | ");
     else
         printf("%4d ", chunk->lines[offset]);
+#endif
 
     const uint8_t instruction = chunk->code[offset];
     switch (instruction)

src/vm.c
@@ -53,7 +53,12 @@ static void runtime_error(const char* format, ...)
         const ObjFunction* func  = frame->closure->func;
 
         const size_t instruction = frame->ip - func->chunk.code - 1;
-        fprintf(stderr, "[line %d] in ", func->chunk.lines[instruction]);
+        fprintf(stderr, "[line %d] in ",
+#ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+                get_line(&func->chunk, instruction));
+#else
+                func->chunk.lines[instruction]);
+#endif
         if (func->name == NULL)
             fprintf(stderr, "script\n");
         else

commit 5ad9fbd2efb5dd8b9e94c172a93fb40750a80f39

chunk: Fix writing line info in the old path
Accidentally changed the order when run-length encoding was implemented.

chunk->count needs to be incremented as the first thing in the new path since
the function can now return early if the passed in line matches previous line.
Count still needs to be incremented in that case.

This is not so for the old path, where each byte in the chunk code array has a
corresponding line info in the lines array, at the same index. Count must be
incremented after storing both of them.

Rename line_start to be more descriptive as well.

Fixes: 64c33f5cbcc355d827b4bf578f487f9473cc1bf2 ("chunk: Implement run-length encoding for line information")

Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

src/chunk.c
@@ -47,9 +47,10 @@ void write_chunk(Chunk* chunk, uint8_t byte, int line)
     }
 
     chunk->code[chunk->count] = byte;
-    chunk->count++;
 
 #ifdef CHUNK_LINE_RUN_LENGTH_ENCODING
+    chunk->count++;
+
     if (chunk->line_count > 0 && chunk->lines[chunk->line_count - 1].line == line)
         return;
 
@@ -61,11 +62,12 @@ void write_chunk(Chunk* chunk, uint8_t byte, int line)
                                             old_capacity, chunk->line_capacity);
     }
 
-    LineStart* line_start = &chunk->lines[chunk->line_count++];
-    line_start->offset    = chunk->count - 1;
-    line_start->line      = line;
+    LineStart* next_line = &chunk->lines[chunk->line_count++];
+    next_line->offset    = chunk->count - 1;
+    next_line->line      = line;
 #else
     chunk->lines[chunk->count] = line;
+    chunk->count++;
 #endif
 }
 